7c2d610a308352b3ba258990218c62cd1c67a09e
Yishai Zehavi <yishaizehavi@yishais-mbp.mynet>
2020-02-24 19:56:03 +0200

    config ava and installed required packages

---
+++ ava.config.js
@@ -0,0 +1,11 @@
+export default {
+    extensions: [
+        "ts"
+    ],
+    require: [
+        "ts-node/register/transpile-only"
+    ],
+    nodeArguments: [
+        "--no-warnings"
+    ]
+}
